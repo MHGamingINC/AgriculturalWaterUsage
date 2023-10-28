e950f1bd35d12456221ba6b737fb4572f82d0b81
DannyJXia <dannyxia123@gmail.com>
2023-10-28 17:05:12 -0400

    Add button function

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 document.getElementById("myButton").addEventListener("click", calculate);
 
-function calculate(waterUsageDict, cropYieldDict, crop, acres){
-    alert("hello");
-    document.getElementById("outputValue").textContent = "cool.";
-    return waterUsageDict.crop * cropYieldDict.crop * acres;
-}
-
-const cropWaterUsage = {
+function calculate(crop, acres){
+  const cropWaterUsage = {
     // Source: chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://hess.copernicus.org/articles/15/1577/2011/hess-15-1577-2011.pdf
     // Measurements in m^3/ton
     corn : 1222,
@@ -17,14 +12,30 @@ const cropWaterUsage = {
 }
 
 const cropYieldsPerAcre = {
-    // Source: https://goodineverygrain.ca/2021/08/26/how-many-pounds-of-grain-can-one-acre-produce/
-    // Measurements in tons/acre
-    corn: 4.14,
-    wheat: 2.3,
-    soy: 1.27,
-    potatoes: 10,
-    rice: 3.69
+  // Source: https://goodineverygrain.ca/2021/08/26/how-many-pounds-of-grain-can-one-acre-produce/
+  // Measurements in tons/acre
+  corn: 4.14,
+  wheat: 2.3,
+  soy: 1.27,
+  potatoes: 10,
+  rice: 3.69
 }
+
+    selectElement = document.querySelector('#cropType');
+    cropType1 = selectElement.value;
+    alert(cropType1);
+    selectElement1 = document.querySelector('#fieldSize');
+    fieldSize1 = selectElement1.value;
+    alert(fieldSize1);
+    alert(cropWaterUsage[cropType1]);
+    alert(cropYieldsPerAcre[cropType1] * fieldSize1);
+
+    document.getElementById("outputValue").textContent = (cropWaterUsage[cropType1] * cropYieldsPerAcre[cropType1] * fieldSize1);
+}
+
+
+
+
 
 let cropForm = document.getElementById("form");
 
